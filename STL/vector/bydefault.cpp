8f1645cc68d87a0281995b2df0ec34e9f12994cf
Kunal Agarwal <mrkunalagarwal33@gmail.com>
2025-09-25 09:15:50 +0530

    Create bydefault.cpp

---
+++ STL/vector/bydefault.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v(5, -1);
+
+    for (int i = 0; i < v.size(); ++i)
+    {
+        cout << v[i] << " ";
+    }
+}
